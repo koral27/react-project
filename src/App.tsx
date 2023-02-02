2b3f6dfc7e79180a8aee1653461b75ed1e54a454
Aleksandr Kornetov <aakornetov@yandex.ru>
2023-02-02 23:04:29 +0300

    Style organization, add theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 
+import "./styles/index.scss"
+import { useTheme } from './theme/useTheme';
+
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to='/'>Main</Link>
       <Link to='about'>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
