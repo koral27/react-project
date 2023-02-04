3ea080a2d3b6297f18fee5be8ba5263d588330cb
Aleksandr Kornetov <aakornetov@yandex.ru>
2023-02-04 15:32:31 +0300

    Organize project architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 
 import "./styles/index.scss"
-import { useTheme } from './theme/useTheme';
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {classNames} from "shared/lib";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +17,8 @@ const App = () => {
       <Link to='about'>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageLazy />} />
-          <Route path='/' element={<MainPageLazy />}/>
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/' element={<MainPage />}/>
         </Routes>
       </Suspense>
     </div>
